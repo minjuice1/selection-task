42e71b640238a305676d9d0005ca695767e767f8
minjuice1 <minjuice93@gmail.com>
2022-08-14 22:56:30 +0900

    [init] 초기세팅 (환경 세팅, 디렉토리 구조 설계)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/auth/login/Login";
+import Signup from "./pages/auth/signup/Signup";
+import Todo from "./pages/todos/todo/Todo";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/login' element={<Login />} />
+				<Route path='/signup' element={<Signup />} />
+				<Route path='/' element={<Todo />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
