53263331ec7b400e5cf4648039a22cabbaa4b731
minjuice1 <minjuice93@gmail.com>
2022-08-18 23:05:45 +0900

    [style] signUp ui 구현

--- src/pages/auth/signup/Signup.jsx
+++ src/pages/auth/signup/Signup.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import styles from "./SignUp.module.css";
 
-const Signup = () => {
-	return <div>Signup</div>;
+const SignUp = () => {
+	return (
+		<div className={styles.container}>
+			<h1 className={styles.title}>SignUp</h1>
+			<form className={styles.userForm}>
+				<div>
+					<label htmlFor='email'>Email </label>
+					<input type='email' id='email' />
+				</div>
+				<div>
+					<label htmlFor='password'>password </label>
+					<input type='password' id='password' autoComplete='off' />
+				</div>
+				<div>
+					<label htmlFor='passwordConfirm'>password Confirm </label>
+					<input type='password' id='passwordConfirm' autoComplete='off' />
+				</div>
+				<input className={styles.submitButton} type='button' value='submit' />
+			</form>
+		</div>
+	);
 };
 
-export default Signup;
+export default SignUp;
