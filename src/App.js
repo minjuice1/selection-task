023724a5ebd89abfd945e0826d56e8da0d2bf60a
minjuice1 <minjuice93@gmail.com>
2022-08-20 01:13:53 +0900

    [add] 1.로그인, 회원가입 시 유효성검사, 2.로컬 스토리지 저장, 3.유효하지 않은 토큰일 경우 알맞은 루트로 리다이렉트

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import styles from "./App.module.css";
 import Login from "./pages/auth/login/Login";
 import SignUp from "./pages/auth/signup/SignUp";
-import Todo from "./pages/todos/todo/Todo";
+import ToDo from "./pages/todos/todo/ToDo";
+import NotFound from "./pages/not_found/NotFound";
+import TodoDetail from "./pages/todos/todo_item/TodoDetail";
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path='/login' element={<Login />} />
-				<Route path='/signup' element={<SignUp />} />
-				<Route path='/' element={<Todo />} />
-			</Routes>
-		</BrowserRouter>
+		<div className={styles.app}>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<ToDo />}>
+						<Route path='todo/:userId' element={TodoDetail} />
+					</Route>
+					<Route path='/login' element={<Login />} />
+					<Route path='/signup' element={<SignUp />} />
+					<Route path='*' element={<NotFound />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
 	);
 }
 
