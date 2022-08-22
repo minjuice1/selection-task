7d6e9e88978fee4e739e77b84ab76593d94ead7b
minjuice1 <minjuice93@gmail.com>
2022-08-22 18:00:51 +0900

    [chore] api 연결중

---
+++ src/services/user.js
@@ -0,0 +1,11 @@
+// import axios from "axios";
+
+// const api = axios.create({
+// 	baseURL:
+// 		"https://5co7shqbsf.execute-api.ap-northeast-2.amazonaws.com/production",
+// });
+
+// export default userFetch = {
+// 	signup: (params) => api.post(`auth/signup`, params),
+// 	signin: (params) => api.post(`auth/signin`, params),
+// };
