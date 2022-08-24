aa931de9c9b9b933786dac119763475814ca378f
minjuice1 <minjuice93@gmail.com>
2022-08-24 14:26:26 +0900

    [styles] notFound page

--- src/pages/not_found/NotFound.jsx
+++ src/pages/not_found/NotFound.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./NotFound.module.css";
 
 const NotFound = () => {
-	return <div>NotFound</div>;
+	let navigate = useNavigate();
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.title}>유효하지 않는 페이지입니다.</div>
+			<button onClick={() => navigate("/")} className={styles.home}>
+				Home
+			</button>
+		</div>
+	);
 };
 
 export default NotFound;
