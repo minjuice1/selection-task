f658f4f49c979cb5b6ead2e20fdbbd67ac352c30
minjuice1 <minjuice93@gmail.com>
2022-09-20 17:30:51 +0900

    [add] useRefreshToken hook 생성

---
+++ src/hooks/useRefreshToken.js
@@ -0,0 +1,24 @@
+import axios from "../api/axios";
+import { useRecoilState } from "recoil";
+import { useAuth } from "context/RecoilProvider";
+
+const useRefreshToken = () => {
+	const [auth, setAuth] = useRecoilState(useAuth);
+	console.log(auth);
+
+	const refresh = async () => {
+		const res = await axios.get("/refresh", {
+			withCredentials: true,
+		});
+		setAuth((prev) => {
+			console.log(JSON.stringify(prev.accessToken));
+			console.log(res.data.accessToken);
+			return { ...prev, accessToken: res.data.accessToken };
+		});
+		return res.data.accessToken;
+	};
+
+	return refresh;
+};
+
+export default useRefreshToken;
