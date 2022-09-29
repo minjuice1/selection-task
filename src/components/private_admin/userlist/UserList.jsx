becc23af34905602f06ab88f225e4ac515f3ded0
minjuice1 <minjuice93@gmail.com>
2022-09-29 11:30:51 +0900

    [add, update] 1. nav의 home버튼, admin일 경우 admin mode 표시 2. admin 컴포넌트와 userList 컴포넌트 구조, 코드수정

--- src/components/private_admin/userlist/UserList.jsx
+++ src/components/private_admin/userlist/UserList.jsx
@@ -1,14 +1,8 @@
-import React, { useEffect, useState } from "react";
 // import getUsersFetch from "services/user";
+import React from "react";
 import styles from "./UserList.module.css";
-import { useLocation, useNavigate } from "react-router-dom";
-import { axiosPrivate } from "api/axios";
-import useAuthState from "hooks/useAuthState";
 
-const Users = () => {
-	const [users, setUsers] = useState();
-	const auth = useAuthState();
-
+const Users = ({ user, id }) => {
 	/* getUserFetch 분리 시도 */
 
 	// useEffect(() => {
@@ -30,38 +24,10 @@ const Users = () => {
 	// 	};
 	// }, []);
 
-	const navigate = useNavigate();
-	const location = useLocation();
-
-	useEffect(() => {
-		let isMounted = true;
-
-		const getUsers = async () => {
-			try {
-				const token = auth?.accessToken;
-				const response = await axiosPrivate.get("/users", {
-					headers: { Authorization: `Bearer ${token}` },
-					withCredentials: true,
-				});
-				console.log(response.data);
-				isMounted && setUsers(response.data);
-			} catch (err) {
-				console.error(err);
-				navigate("/login", { state: { from: location }, replace: true });
-			}
-		};
-
-		getUsers();
-
-		return () => {
-			isMounted = false;
-		};
-	}, []);
-
 	const whichRole = (user) => {
 		if (user?.roles?.Admin) {
 			return "Admin";
-		} else if (users?.roles?.Manager) {
+		} else if (user?.roles?.Manager) {
 			return "Manager";
 		} else {
 			return "User";
@@ -70,22 +36,11 @@ const Users = () => {
 
 	return (
 		<>
-			<article className={styles.container}>
-				<h2 className={styles.title}>List of all user </h2>
-				{users?.length ? (
-					<ol>
-						{users.map((user, i) => (
-							<li key={i}>
-								<a className={styles.id}>ID : {user?.username}</a>
-								<br />
-								<span className={styles.role}>ROLE : {whichRole(user)}</span>
-							</li>
-						))}
-					</ol>
-				) : (
-					<p className={styles.nolist}>User list does not exist.</p>
-				)}
-			</article>
+			<li key={id}>
+				<a className={styles.id}>ID : {user?.username}</a>
+				<br />
+				<span className={styles.role}>ROLE : {whichRole(user)}</span>
+			</li>
 		</>
 	);
 };
--- src/pages/private_pages/admin/Admin.jsx
+++ src/pages/private_pages/admin/Admin.jsx
@@ -1,14 +1,60 @@
+import React, { useEffect, useState } from "react";
 import UserList from "components/private_admin/userlist/UserList";
-import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { axiosPrivate } from "api/axios";
+import useAuthState from "hooks/useAuthState";
 import styles from "./Admin.module.css";
 
 const Admin = () => {
+	const [users, setUsers] = useState();
+	const auth = useAuthState();
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	useEffect(() => {
+		let isMounted = true;
+
+		const getUsers = async () => {
+			try {
+				const token = auth?.accessToken;
+				const response = await axiosPrivate.get("/users", {
+					headers: { Authorization: `Bearer ${token}` },
+					withCredentials: true,
+				});
+				console.log(response.data);
+				isMounted && setUsers(response.data);
+			} catch (err) {
+				console.error(err);
+				navigate("/login", { state: { from: location }, replace: true });
+			}
+		};
+
+		getUsers();
+
+		return () => {
+			isMounted = false;
+		};
+	}, []);
+
 	return (
 		<div className={styles.container}>
 			<h1> Admin Page</h1>
 			<div className={styles.section}>
-				<UserList />
-				<div className={styles.todo} />
+				<article className={styles.userListBox}>
+					<h2 className={styles.title}>List of all user </h2>
+					{users?.length ? (
+						<ol>
+							{users.map((user, id) => (
+								<UserList key={id} user={user} />
+							))}
+						</ol>
+					) : (
+						<p className={styles.nolist}>User list does not exist.</p>
+					)}
+				</article>
+				<div className={styles.todo}>
+					<article></article>
+				</div>
 			</div>
 		</div>
 	);
